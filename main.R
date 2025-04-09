a85443024ee6b7cfeb5d4698320a21a8c705ad08
Shahab Jolani <shahabjolani@users.noreply.github.com>
2025-04-09 17:48:15 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,8 @@
 pack <- lapply(c("mvtnorm", "mice", "MASS", "lme4", "msm", "broom.mixed", "micemd", "tidyverse"), require, character.only = TRUE)
 
+source("functions.R")
+source("mlmi-binomial.R")
+source("mlmi-poisson.R")
+
 res.sim <- sim(rep = 2, k = 20, model = "bin", outcome = "bin", sys = .1, omega = "moderate")
 extract(res.sim)
