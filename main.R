2d0a8660b44c646ef8f3188aef300ca477e2c8cd
Shahab Jolani <shahabjolani@users.noreply.github.com>
2025-04-10 14:35:15 +0200

    Update main.R

--- main.R
+++ main.R
@@ -4,5 +4,7 @@ source("functions.R")
 source("mlmi-binomial.R")
 source("mlmi-poisson.R")
 
+# this function runs the specified simulation scenario with 2 replications 
 res.sim <- sim(rep = 2, k = 20, model = "bin", outcome = "bin", sys = .1, omega = "moderate")
+# this functino extracts the corresponding results 
 extract(res.sim)
