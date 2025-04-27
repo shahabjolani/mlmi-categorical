6527266fe726fc99ff7da20e2957559da628f1f9
Shahab Jolani <shahabjolani@users.noreply.github.com>
2025-04-27 22:53:45 +0200

    Add files via upload

---
+++ application.R
@@ -0,0 +1,27 @@
+# The list of individual participant datasets are as follows:
+# study 1: https://doi.org/10.1371/journal.pone.0214776
+# study 2: https://doi.org/10.1371/journal.pone.0171009
+# study 3: https://doi.org/10.1371/journal.pone.0222236
+# study 4: https://doi.org/10.1371/journal.pone.0172241
+# study 5: https://doi.org/10.1371/journal.pone.0172241
+# study 6: https://doi.org/10.1371/journal.pone.0127071
+# study 7: https://doi.org/10.1371/journal.pone.0138461
+# study 8: https://doi.org/10.1371/journal.pone.0230020
+# study 9: https://doi.org/10.1371/journal.pone.0168117
+# study 10: https://doi.org/10.1371/journal.pone.0128994
+# study 11: https://doi.org/10.1371/journal.pone.0173799
+# study 12: https://doi.org/10.1371/journal.pone.0266617
+# study 13: https://doi.org/10.1371/journal.pone.0278563
+# study 14: https://doi.org/10.1371/journal.pone.0144121
+# study 15: https://doi.org/10.1371/journal.pone.0100835
+
+# Note that the variable names and coding schemes (for binary variables) are not unique.   
+
+# Load 'ipdresult.RData' to produce Tables 4 and 5
+load(ipdresult)
+
+# Table 4
+ipdresult$perc
+
+# Table 5
+ipdresult$result
